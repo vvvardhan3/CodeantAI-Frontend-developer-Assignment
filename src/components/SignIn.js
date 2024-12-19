ad1b904e4479686697dc3d21a2cf627dc3eb9688
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-12-20 00:59:47 +0530

    Next steps comments

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -9,13 +9,16 @@ function SignIn() {
         <div>
         <img
           className="logo"
+        //   Verify this image code
           src={require("../assests/logo.png")}
           alt="group_Image"
         />
         <h1>Welcome to CodeAnt AI</h1>
 
         <div className="btn">
+            {/* Next Step, Create a useState variable, dynamic css properties and onclick mechanism as well */}
             <button className= {login ? "button" : ""} onClick={login}>SAAS</button>
+            {/* Modify the code here */}
             <button className= {!login ? "button" : ""} onClick={setLogin}> Self Hosted</button>
         </div>
 
